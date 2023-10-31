bad80bdf091b646c52cb91a632ce4c5cdee62e15
lch09253 <ch.lee@theinfoin.com>
2023-10-31 16:03:44 +0900

    feat: 다크모드 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "./App.css";
+import React, { createContext, useContext, useState } from "react";
 
-function App() {
+const DarkModeContext = createContext();
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <>
-      <h1>useContext</h1>
-    </>
+    <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      <Children />
+    </DarkModeContext.Provider>
   );
-}
+};
 
 export default App;
+
+const Children = () => {
+  const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
+
+  const handleToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: isDarkMode ? "#000" : "#fff",
+        color: isDarkMode ? "#fff" : "#000",
+      }}
+    >
+      <h1>useContext</h1>
+      <button onClick={handleToggleDarkMode}>
+        {isDarkMode ? "지금은 다크모드" : "지금은 라이트모드"}
+      </button>
+    </div>
+  );
+};
